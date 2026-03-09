6cf1d82e53cd8f233fb4f7b279cbb860b3ea1a94
b0tm4r <76016646+b0tm4r@users.noreply.github.com>
2026-03-09 11:38:21 +0000

    update·

---
+++ workflow-app/src/main/java/com/example/workflow/CamundaApplication.java
@@ -0,0 +1,40 @@
+package com.example.workflow;
+
+import org.camunda.bpm.engine.*;
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+@EnableProcessApplication("workflow-app")
+public class CamundaApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(CamundaApplication.class, args);
+  }
+
+  @Bean
+  public CommandLineRunner inspectEngine(ProcessEngine processEngine) {
+    return args -> {
+
+      System.out.println("Process Engine Name: " + processEngine.getName());
+
+      RuntimeService runtimeService = processEngine.getRuntimeService();
+      TaskService taskService = processEngine.getTaskService();
+      RepositoryService repositoryService = processEngine.getRepositoryService();
+      HistoryService historyService = processEngine.getHistoryService();
+      ManagementService managementService = processEngine.getManagementService();
+
+      System.out.println("RuntimeService: " + runtimeService);
+      System.out.println("TaskService: " + taskService);
+      System.out.println("RepositoryService: " + repositoryService);
+      System.out.println("HistoryService: " + historyService);
+      System.out.println("ManagementService: " + managementService);
+
+    };
+  }
+
+}
---
+++ workflow-app/src/main/java/com/example/workflow/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.example.workflow;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "workflow-app"; // BPMN Process ID
+
+}
